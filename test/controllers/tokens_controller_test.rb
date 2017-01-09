877d042c3edb320279430f9956629823556a5a3e
EDLyonhart <EDLyonhart@gmail.com>
2017-01-08 17:59:11 -0800

    update tokens controller tests

--- test/controllers/tokens_controller_test.rb
+++ test/controllers/tokens_controller_test.rb
@@ -1,24 +1,18 @@
 require 'test_helper'
 
 class TokensControllerTest < ActionDispatch::IntegrationTest
-  # test "should get new" do
-  #   get tokens_new_url
-  #   assert_response :success
-  # end
+  setup do
+    @token = tokens(:one)
+  end
 
-  # test "should get create" do
-  #   get tokens_create_url
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get new_token_path
+    assert_response :success
+  end
 
-  # test "should get update" do
-  #   get tokens_update_url
-  #   assert_response :success
-  # end
-
-  # test "should get destroy" do
-  #   get tokens_destroy_url
-  #   assert_response :success
-  # end
+  test "should get create" do
+    get new_token_path
+    assert_response :success
+  end
 
 end
