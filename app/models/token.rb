c84dc222070bb392eb7dcdf20c60353d7cb7aa34
EDLyonhart <EDLyonhart@gmail.com>
2017-01-11 16:05:51 -0800

    add to token model spec tests

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,13 +1,11 @@
 class Token < ApplicationRecord
 
   belongs_to :user
-  # after_initialize :initialize_defaults, :if => :new_record?
-
 
   def self.generate(user)
 
     if Token.exists?(:user_id => user.id)
-      p "User already has an associated token"
+      return "User already has an associated token"
     else
       token = Token.new
       token.nonce = SecureRandom.base64(64)
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -7,18 +7,34 @@ RSpec.describe Token, type: :model do
     @token = Token.generate(@user)
   end
 
-  context "generate static method should" do
-    it "increase the number of tokens" do
+  context "generate static method" do
+    it "should increase the number of tokens" do
       expect Token.count == @init_token_count+1
     end
 
-    it "associate correct user_id" do
+    it "should associate correct user_id" do
       expect @user.id == @token.user_id
+    end
+
+    it "should only create a token for users who dont have one associated" do
+      # Inital token count has already been incremented once above
+      expect Token.count != @init_token_count+2
+      expect Token.generate(@user) == "User already has an associated token"
     end
   end
 
+context "consume static method" do
+  it "should delete the token and return associated user" do
+    expect Token.consume(@token.nonce) == @user
+    expect Token.count == @init_token_count
+  end
+
+  it "should return nil for a token which cant be found" do
+    expect Token.consume(@token.nonce) == nil
+  end
+end
+
   after(:example) do
     @user.delete
-    @token.delete
   end
 end
