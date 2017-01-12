4d70945b041cbdfdae9448ff9a7169d63ee9b01c
EDLyonhart <EDLyonhart@gmail.com>
2017-01-11 17:20:47 -0800

    remove initial values for messaging preferences

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,12 @@ class User < ApplicationRecord
   validates :email, :presence => true, 
                     :length => { :minimum => 3, :maximum => 254 },
                     :uniqueness => true
-                    # :email => true
   
   store :messaging_preferences, accessors: [:marketing, :articles, :digest]
 
-  after_initialize :initialize_defaults, :if => :new_record?
-
   def confirm_token
     @token = Token.find_by(user_id: self.id)
-    if (@token.nonce == nil || User.find(@token.user_id).email != self.email)
+    if (@token == nil || @token.nonce == nil || User.find(@token.user_id).email != self.email)
       return false
     else
       self.unsubscribe_from_all
@@ -23,15 +20,7 @@ class User < ApplicationRecord
   end
 
   def unsubscribe_from_all
-    self.update_attributes!(messaging_preferences: {"marketing"=>"false", "articles"=>"false", "digest"=>"false"})
+    self.update_attributes!(messaging_preferences: {"marketing"=>false, "articles"=>false, "digest"=>false})
   end
-
-  private
-
-    def initialize_defaults
-      self.marketing = false
-      self.articles = false
-      self.digest = false
-    end
 
 end
